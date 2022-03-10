6c2bd878f6706627dca9ae8c5004fd9bdf166d69
abdulwahabkhalid <abdulwahabkhalid29@gmail.com>
2022-03-09 12:52:07 -1200

    Php Working

---
+++ february/else.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>else</title>
+</head>
+<body>
+    <h1 style="color:blue;">Elseif NO # 2</h1>
+    <?php
+    $a = 5;
+    $b = 10;
+
+    if ($a == $b) {
+        echo "a is equal to b!";
+    }
+    
+    elseif ($a != $b) {
+        echo "a is equal to b!";
+    }
+    else  {
+        echo "Have a good night!";
+      }
+    
+    ?>
+</body>
+</html>
---
+++ february/elseif.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1 style="color:red;">Elseif NO # 1</h1>
+<?php
+
+$t = 4;
+if ($t) {
+  echo "date is 7/3/2022.";
+} elseif ($t) {
+  echo "date is 7/3/2022.";
+} else {
+  echo "date is 7/3/2022.";
+}
+?>
+ 
+</body>
+</html>
